e5adfd22226651fa3dee542e54aa8582e50ceaf6
Sam <samarthsbhat05@gmail.com>
2026-02-15 15:07:18 +0530

    Time: 8 ms (42.56%) | Memory: 13.7 MB (45.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,6 +5,7 @@ class Solution(object):
             if nums[i]!=0:
                 nums[insert_pos],nums[i]=nums[i],nums[insert_pos]
                 insert_pos+=1
+
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
