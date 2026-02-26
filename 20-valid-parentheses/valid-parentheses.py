a159863a956e11768b353f0977c8864115b49a9a
Sam <samarthsbhat05@gmail.com>
2026-02-26 06:36:06 +0530

    Time: 4 ms (45.27%) | Memory: 12.7 MB (9.91%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        mapping={
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        for ch in s:
+            if ch in mapping:
+                if not stack or stack[-1]!=mapping[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+        return not stack
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
