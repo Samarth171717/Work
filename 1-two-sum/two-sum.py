cdeb63630b82f003567d1cabc5988549f2c7e496
Sam <samarthsbhat05@gmail.com>
2026-08-21 13:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (17.78%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen={}
         for i in range(len(nums)):
             compliment=target-nums[i]
             if compliment in seen:
                 return [seen[compliment],i]
             seen[nums[i]]=i
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
